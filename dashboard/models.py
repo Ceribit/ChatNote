1822dd6b7e36f776a80053158b9a53a35d24f941
Irec <ceribit@gmail.com>
2018-07-07 00:32:41 -0500

    Added login functionality

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 
-# Create your models here.
+# Test model
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    description = models.CharField(max_length = 200)
+    created_at = models.DateTimeField(auto_now_add = True)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name + 'by' + user
+
+class Account(models.Model):
+    nickname = models.CharField(max_length=24, blank=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    birthdate = models.DateField()
+    def __str__(self):
+        return 'Name: ' + user
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    #if not logged in
+    if not request.user.is_authenticated:
+        form = AuthenticationForm()
+        if request.method == 'GET':
+            return move_home(request)
+
+        elif request.method == 'POST':
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                login(request, form.get_user())
+                return render(request, 'dashboard/home.html')
+        return move_home(request)
+    else:
+        print('You are logged in')
+        return render(request, 'dashboard/home.html')
+    #if not logged in
+
+def move_home(request):
+    form = AuthenticationForm(request)
+    return render(request, 'registration/login.html', {'form':form})
